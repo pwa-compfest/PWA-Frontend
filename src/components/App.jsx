5108f932ca7c60f43708cb47b6cdabbe681c6197
fajarbh <hajargak@gmail.com>
2022-08-14 18:30:28 +0700

    fix landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import LandingPage from "../pages/LandingPage";
 import ForgotPassword from "../pages/ForgotPassword";
-import RegisteredPage from "../pages/RegisteredPage";
+// import RegisteredPage from "../pages/RegisteredPage";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -13,10 +13,11 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <div className="content py-2 h-[80vh] md:h-[77.4vh] md:relative sm:px-[100px] mt-8">
+        <div className="content h-[80vh] md:h-[77.4vh] md:relative sm:px-[100px] mt-8">
           <Routes>
             <Route exact path="/" element={<LandingPage />} />
             <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
         </div>
