1ccf1e68cb322c58f95a3a04db3e0421f686c69b
BrianKheng <briankheng63@gmail.com>
2022-08-14 19:32:28 +0700

    fix signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,56 +1,50 @@
 import React from "react";
 function SignIn() {
   return (
-      <div className="grid grid-cols-2 mx-auto">
-        <div className="flex justify-center items-center justify-items-center">
-          <div>
-            <h2 className="h2 flex justify-center mb-4">Welcome Back!</h2>
-            <p className="subtitle flex justify-center text-center text-neutral-500">
+    <section className="bg-white px-[30px]">
+      <div className="container">
+        <div className="grid md:grid-cols-1 xl:grid-cols-2">
+          <div className="justify-center text-center p-5 hidden xl:block">
+            <h2 className="h2">Welcome Back!</h2>
+            <p className="subtitle text-neutral-500 mt-3 mb-7">
               Sign in and let's continue our journey towards free education.
             </p>
-            <div className="hidden w-[400px] justify-center sm:grid items-center ml-[50px]">
-              <img
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-                alt="mockup"
-              />
-            </div>
+            <img src="/images/sign_in.png" alt="mockup" />
           </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="mx-auto place-self-center lg:col-span-7 rounded-2xl shadow-xl px-6 py-6">
+          <div className="justify-center max-w-md mx-auto md:max-w-2xl w-[500px] p-12 place-self-center rounded-[24px] shadow-xl">
             <div>
-              <label className="block text-neutral-500">Email</label>
+              <label className="label-form block">Email</label>
               <input
-                className="label"
-                type={"text"}
+                className="text-input mx-auto focus:outline-none"
+                type={"email"}
                 placeholder="example@email.com"
               ></input>
-            </div>
-            <div>
-              <label className="block mt-5 text-neutral-500">Password</label>
+              <label className="label-form block mt-3">Password</label>
               <input
-                className="label"
-                type={"text"}
+                className="text-input mx-auto focus:outline-none"
+                type={"password"}
                 placeholder="enter password"
               ></input>
-            </div>
-            <div className="flex justify-end mt-3">
-              <a href="/forgot-password" className="link text-primary-300 text-[12px] no-underline">
+              <a
+                href="/forgot-password"
+                className="link text-primary-300 text-[12px] no-underline flex items-end justify-end"
+              >
                 Forgot Password?
               </a>
             </div>
-            <button className="btn-primary block w-[100%] mt-20">
+            <button className="btn-primary block w-[100%] mt-12">
               SIGN IN
             </button>
             <p className="body mt-8 text-neutral-500 flex justify-center">
               Don't have an account yet?
-              <a href="/" className="link text-primary-300 ml-2 no-underline">
+              <a href="/register" className="link text-primary-300 ml-2 no-underline">
                 Create One
               </a>
             </p>
           </div>
         </div>
       </div>
+    </section>
   );
 }
 
